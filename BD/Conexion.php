68e7cbff511c76d5ab992f9f2ff82e72f1e96f97
German <tobito.987@gmail.com>
2024-10-16 09:10:48 -0500

    Solucionando los cambios

--- BD/Conexion.php
+++ BD/Conexion.php
@@ -1,30 +1,31 @@
-<?php 
+<?php
 // Datos para conectar a la base de datos
 const Servidor = "localhost";
 const BaseDeDatos = "bdmundo";
 const Usuario = "root";
 const Pass = "";
-const SGBD = "mysql:host=".Servidor.";dbname=".BaseDeDatos .";charset=UTF8";
+const SGBD = "mysql:host=" . Servidor . ";dbname=" . BaseDeDatos . ";charset=UTF8";
 
-class Conexion 
+class Conexion
 {
-	public static function conectar(){
-		try {
-			
-			$con = new PDO(SGBD,Usuario,Pass);
-			
-			} catch (PDOException $e) {
-			    
-			if($con == false){
-			    
-			    header('Location: ../503.php');
-			    //die("El error de Conexión es :".$e->getMessage());
-			}
-		}
-		return $con;
-	}
-	      //Fin de la funcion conectar
+    public static function conectar()
+    {
+        try {
+
+            $con = new PDO(SGBD, Usuario, Pass);
+
+        } catch (PDOException $e) {
+
+            if ($con == false) {
+
+                header('Location: ../503.php');
+                //die("El error de Conexión es :".$e->getMessage());
+            }
+        }
+        return $con;
+    }
+    //Fin de la funcion conectar
 }//Final de la class conexion
 
 
-		?>
+?>
